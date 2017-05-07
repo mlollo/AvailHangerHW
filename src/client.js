83fa818a6f3a7b953015938f593c2169406f9778
mlollo <mllollo@outlook.fr>
2017-05-07 12:19:13 +0200

    fds

--- src/client.js
+++ src/client.js
@@ -11,26 +11,24 @@ rl.on('line', function(line){
     console.log(line);
     if(line == "setup"){
 		var options = {
-		    hostname: 'http://2bb05442.ngrok.io',
+		    hostname: 'http://ed744996.ngrok.io',
 		    path: '/',
-		    port: 80,
-		    secure: false,
 		    method: 'GET',
 		    headers: {
-		      'Authorization': new Buffer('8D0dZXMIBeiWSj7:YzDVJPRyIUp7DS0').toString('base64')
+		      'Authorization': "authorization " + new Buffer('8D0dZXMIBeiWSj7:YzDVJPRyIUp7DS0').toString('base64'),
 		    }
 		};
-		console.log(options);
+		// console.log(options);
 		  var client = new HTTPClient(options)
-		  client.request('/api/getToken', function (err, res, body) {
+		  client.request('/api/gettoken', function (err, res, body) {
 		    console.log('error', err);
 		    console.log('response', res);
-		    console.log('body', body);
-		    token = body.token;
+		    console.log('body', JSON.parse(body.toString()).token);
+		    token = JSON.parse(body.toString()).token;
 		  });
     }else if(line == "removed"){
 		 var options = {
-		    hostname: 'http://2bb05442.ngrok.io',
+		    hostname: 'http://ed744996.ngrok.io',
 		    path: '/',
 		    port: 80,
 		    secure: false,
@@ -42,11 +40,11 @@ rl.on('line', function(line){
 		  client.request('/api/hanger/:i&token='+token, function (err, res, body) {
 		    console.log('error', err);
 		    console.log('response', res);
-		    console.log('body', body);
+		    console.log('body', JSON.parse(body.toString()));
 		  });
     }else if(line == "deposit"){
  		var options = {
-		    hostname: 'http://2bb05442.ngrok.io',
+		    hostname: 'http://ed744996.ngrok.io',
 		    path: '/',
 		    port: 80,
 		    secure: false,
@@ -58,7 +56,7 @@ rl.on('line', function(line){
 		  client.request('/api/hanger/:i&token='+token, function (err, res, body) {
 		    console.log('error', err);
 		    console.log('response', res);
-		    console.log('body', body);
+		    console.log('body', JSON.parse(body.toString()));
 		  });
     }
 });
